18c204b8de59590caee3063287f2461d013afe3c
LiuchangDuan <870121425@qq.com>
2016-05-04 22:58:36 +0800

    主框架搭建

--- src/com/example/clock/MainActivity.java
+++ src/com/example/clock/MainActivity.java
@@ -1,15 +1,8 @@
 package com.example.clock;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.TabHost;
 
 
 
@@ -19,7 +12,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        tabHost = (TabHost) findViewById(android.R.id.tabhost);
+        tabHost.setup();
+        
+        tabHost.addTab(tabHost.newTabSpec("tabTime").setIndicator("时钟").setContent(R.id.tabTime));
+        tabHost.addTab(tabHost.newTabSpec("tabAlarm").setIndicator("闹钟").setContent(R.id.tabAlarm));
+        tabHost.addTab(tabHost.newTabSpec("tabTimer").setIndicator("计时器").setContent(R.id.tabTime));
+        tabHost.addTab(tabHost.newTabSpec("tabStopWatch").setIndicator("秒表").setContent(R.id.tabStopWatch));
+        
     }
 
+    
+    private TabHost tabHost;
 
 }
